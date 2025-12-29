2b05053074efa4b2cdf1cbbb0f5c9f8c8fc0cb68
Arshdeep singh chahal <Chahalarsh342@gmail.com>
2025-12-29 14:50:06 +0000

    [LeetCode Sync] Runtime - 9 ms (7.75%), Memory - 23.9 MB (55.26%)

---
+++ Leetcode/0283-move-zeroes/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        for(int i = 0; i < nums.size() - j; i++){
+            if(nums[i] == 0){
+                nums.erase(nums.begin() + i);
+                nums.push_back(0);
+                i--;
+                j++;
+            }
+        }
+    }
+};
