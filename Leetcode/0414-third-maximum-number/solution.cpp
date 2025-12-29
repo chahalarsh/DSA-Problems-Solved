7d63364233c3a56113f57a419934f2bff81a32f2
Arshdeep singh chahal <Chahalarsh342@gmail.com>
2025-12-29 14:01:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13 MB (27.21%)

---
+++ Leetcode/0414-third-maximum-number/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        long long max1 = LLONG_MIN;
+        long long max2 = LLONG_MIN;
+        long long max3 = LLONG_MIN;
+
+        for (int n : nums) {
+            if (n == max1 || n == max2 || n == max3) continue;
+
+            if (n > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = n;
+            } else if (n > max2) {
+                max3 = max2;
+                max2 = n;
+            } else if (n > max3) {
+                max3 = n;
+            }
+        }
+
+        return (max3 == LLONG_MIN) ? max1 : max3;
+    }
+};
