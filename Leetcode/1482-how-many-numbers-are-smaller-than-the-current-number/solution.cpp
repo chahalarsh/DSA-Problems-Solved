896976d9de59d7ef544b860171d709a79ac81006
Arshdeep singh chahal <Chahalarsh342@gmail.com>
2026-01-03 11:21:38 +0000

    [LeetCode Sync] Runtime - 10 ms (36.94%), Memory - 14.1 MB (94.72%)

---
+++ Leetcode/1482-how-many-numbers-are-smaller-than-the-current-number/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+        vector<int> res;
+        int count = 0;
+        for(int i = 0; i < nums.size(); i++){
+            count = 0;
+            for(int j = 0; j < nums.size(); j++){
+                if(nums[j] < nums[i]){
+                    count++;
+                }
+            }
+            res.push_back(count);
+        }
+        return res;
+    }
+};
