749354b390759e3527496707e1b665bbd1ff5d0d
Arshdeep singh chahal <Chahalarsh342@gmail.com>
2026-01-03 13:43:57 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 55 MB (34.59%)

---
+++ Leetcode/0448-find-all-numbers-disappeared-in-an-array/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> findDisappearedNumbers(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> mpp(n+1,0);
+        for(int i=0;i<n;i++) mpp[nums[i]]++;
+        vector<int> ans;
+        for(int i=1;i<=n;i++){
+            if(mpp[i]==0) ans.push_back(i);
+        }
+        return ans;
+    }
+};
