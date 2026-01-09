82de769179e9980eccb8cb83b81d3c73b7e1e0c8
Arshdeep singh chahal <Chahalarsh342@gmail.com>
2026-01-09 16:18:46 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 13.5 MB (34.44%)

---
+++ Leetcode/1421-find-numbers-with-even-number-of-digits/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findNumbers(vector<int>& nums) {
+        int res = 0;
+        for(int i = 0; i < nums.size(); i++){
+            int digitCount = 0;
+            while(nums[i] != 0){
+                nums[i] /= 10;
+                digitCount++;
+            }
+            if(digitCount % 2 == 0){
+                res++;
+            }            
+        }
+        return res;
+    }
+};
