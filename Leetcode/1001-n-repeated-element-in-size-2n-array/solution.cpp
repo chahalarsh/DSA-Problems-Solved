9559bfd21badaefed7b315c1a3f66e684ca8de83
Arshdeep singh chahal <Chahalarsh342@gmail.com>
2026-01-12 05:59:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 28.3 MB (95.50%)

---
+++ Leetcode/1001-n-repeated-element-in-size-2n-array/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int repeatedNTimes(vector<int>& nums) {
+        unordered_map<int,int> freq;
+        for(int num: nums){
+            freq[num]++;
+            if(freq[num] > 1){
+                return num;
+            }
+        }
+        return 0;
+    }
+};
