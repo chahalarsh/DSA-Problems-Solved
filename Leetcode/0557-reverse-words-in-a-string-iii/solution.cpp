7e950c0842686fef55d982152cda3d74f8822139
Arshdeep singh chahal <Chahalarsh342@gmail.com>
2026-02-04 16:40:00 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12 MB (98.91%)

---
+++ Leetcode/0557-reverse-words-in-a-string-iii/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+        for(int i = 0; i < s.size(); i++){
+            
+            int j = i;
+
+            while(j < s.size() && s[j] != ' ' ){
+                j++;
+            }
+
+            reverse(s.begin() + i, s.begin() + j); 
+ 
+            i = j;
+        }
+        return s;
+
+    }
+};
