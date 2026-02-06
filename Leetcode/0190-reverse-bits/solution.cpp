a1501eac697bdcbcbeb8879326296edd88fa8e84
Arshdeep singh chahal <Chahalarsh342@gmail.com>
2026-02-06 15:25:50 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.2 MB (45.27%)

---
+++ Leetcode/0190-reverse-bits/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverseBits(int n) {
+        int res = 0;
+        for(int i = 1; i <= 32; i++){
+            res = res << 1;
+
+            if( (n & 1) == 1){
+                res = res | 1;
+            }
+
+            n = n >> 1;
+        }
+        return res;
+    }
+};
