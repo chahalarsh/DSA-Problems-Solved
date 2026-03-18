3eb4479fd9eda1fdc84845511fa1da2148b8c609
Arshdeep singh chahal <Chahalarsh342@gmail.com>
2026-03-18 10:20:12 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 9 MB (48.86%)

---
+++ Leetcode/0119-pascals-triangle-ii/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        vector<int> res;
+        res.push_back(1);
+        long long prev = 1;
+        for(int i = 1; i <= rowIndex; i++){
+            long long nextVal = prev * ( rowIndex - i + 1) / i;
+            res.push_back(nextVal);
+            prev = nextVal;
+        }
+        return res;        
+    }
+};
