64ec31d2f21e6b250c0cbc971df41de8b9338c9d
Arshdeep singh chahal <Chahalarsh342@gmail.com>
2026-03-24 15:13:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 29.5 MB (92.28%)

---
+++ Leetcode/1839-decode-xored-array/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) {
+        vector<int> res( encoded.size() + 1);
+
+        res[0] = first;
+
+        for(int i = 0; i < res.size() - 1; i++){
+            res[i + 1] = encoded[i] ^ res[i];
+        }
+        return res;
+    }
+};
