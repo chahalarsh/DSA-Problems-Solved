475c9e99c615e0a21e2b8ab2ebaee06151b5f695
Arshdeep singh chahal <Chahalarsh342@gmail.com>
2026-09-09 06:43:42 +0000

    [LeetCode Sync] Runtime - 1 ms (79.58%), Memory - 8.6 MB (68.62%)

---
+++ Leetcode/4297-sum-of-primes-between-number-and-its-reverse/solution.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+
+    int sumOfPrimesInRange(int n) {
+        int r = reverseNum(n);
+
+        int low = min(n, r);
+        int high = max(n, r);
+
+        int sum = 0;
+
+        for (int i = low; i <= high; i++) {
+            if (isPrime(i)) {
+                sum += i;
+            }
+        }
+
+        return sum;
+    }
+
+    bool isPrime(int x) {
+        if (x <= 1) return false;
+        if (x == 2) return true;
+        if (x % 2 == 0) return false;
+
+        for (int i = 3; i * i <= x; i += 2) {
+            if (x % i == 0) return false;
+        }
+        return true;
+    }
+    
+    int reverseNum(int n) {
+        int r = 0;
+        while (n > 0) {
+            r = r * 10 + (n % 10);
+            n /= 10;
+        }
+        return r;
+    }
+};
