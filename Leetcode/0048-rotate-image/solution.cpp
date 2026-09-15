72d367e09041dfe2032031f7395badb23004570f
Arshdeep singh chahal <Chahalarsh342@gmail.com>
2026-09-15 12:54:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.2 MB (35.48%)

---
+++ Leetcode/0048-rotate-image/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        reverse( matrix.begin(), matrix.end());
+
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                if( j >= i){
+                    swap( matrix[i][j], matrix[j][i]);
+                }      
+            }
+        }
+        
+    }
+};
