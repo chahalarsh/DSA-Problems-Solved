e473f85d559e68ea933366dcff761b9c236c65e4
Arshdeep singh chahal <Chahalarsh342@gmail.com>
2026-09-23 10:25:58 +0000

    [LeetCode Sync] Runtime - 2 ms (18.31%), Memory - 27.3 MB (16.19%)

---
+++ Leetcode/0042-trapping-rain-water/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int res = 0;
+        int n = height.size();
+
+        vector<int> lMax(n, 0);
+        vector<int> rMax(n, 0);
+
+        lMax[0] = height[0];
+        for (int i = 1; i < n; i++) {
+            lMax[i] = max(lMax[i - 1], height[i]);
+        }
+
+        rMax[n - 1] = height[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rMax[i] = max(rMax[i + 1], height[i]);
+        }
+
+        for(int i = 0; i < n; i++){
+            res += min(lMax[i], rMax[i]) - height[i];
+        }
+        
+        return res;
+    }
+};
